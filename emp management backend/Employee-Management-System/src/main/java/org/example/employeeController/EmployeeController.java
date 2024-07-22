85016376dce67e65c8b690e161a22b74aa286677
KasunChethiya <kcrathnamalala@gmail.com>
2024-07-23 03:33:57 +0530

    added get and post mapping

---
+++ emp management backend/Employee-Management-System/src/main/java/org/example/employeeController/EmployeeController.java
@@ -0,0 +1,24 @@
+package org.example.employeeController;
+
+import org.example.dto.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/emp-controller")
+public class EmployeeController {
+    List<Employee> employeeList= new ArrayList();
+    @PostMapping("add-employee")
+    public void addEmployee(@RequestBody Employee employee){
+        employeeList.add(employee);
+
+    }
+    @GetMapping("get-all")
+    public List<Employee> getAll(){
+        return employeeList;
+    }
+}
+
+//http:localhost:8080/emp-controller
